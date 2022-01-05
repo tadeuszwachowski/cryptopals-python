6eda3b9bf80a88613872eefcf9d6d0f2b419e302
qualorm <qualorm@protonmail.com>
2022-01-05 22:42:18 +0100

    Finished PKCS7

--- s02/PKCS7_PADDING.py
+++ s02/PKCS7_PADDING.py
@@ -1,1 +1,18 @@
-#TODO Implement PKCS#7 padding https://cryptopals.com/sets/2/challenges/9
+def pkms(message,block_size):
+    b = message.encode('ascii')
+    diff = block_size - len(b)
+    b += bytes([diff])*diff
+    print(b)
+        
+
+
+def main():
+    msg = "YELLOW SUBMARINE"
+    block_size = 20
+    
+    pkms(msg,block_size)
+    
+
+
+if __name__ == '__main__':
+    main()
